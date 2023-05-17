67d7b3e9a7dc0e0431cd938e45c0fab2e09a054a
Brian Davis <badavis95@hotmail.com>
2023-05-16 23:19:32 -0400

    Add files via upload

---
+++ visit_counter_spec.js
@@ -0,0 +1,18 @@
+describe('Visit Counter Test', () => {
+    it('Checks if the visit counter is updated', () => {
+      // Replace the path with the correct path to your index.html file
+      cy.visit('https://bdavisawsresume.link');
+  
+      cy.window()
+        .its('updateCounter')
+        .invoke('call', null);
+  
+      cy.get('#Visit_Count')
+        .should(($span) => {
+          const visitCount = parseInt($span.text(), 10);
+          expect(visitCount).to.be.a('number');
+          expect(visitCount).to.be.at.least(1);
+        });
+    });
+  });
+  
